8bb62de83a99ffab32e07679cd1a911529dcf846
Danys Guevara <ga19014@ues.edu.sv>
2023-10-18 16:23:29 -0600

    Version 1.0 DanysDev

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    <title></title>
+</head>
+<body>
+    @yield('titulo')
+    <p>hgvjvjjhv</p>
+    @stop
+</body>
+</html>
