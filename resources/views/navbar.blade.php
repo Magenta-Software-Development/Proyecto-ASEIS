2803bc2f316fadc85e3bca09d5b1bb148b5b0770
Diegolazo32 <diegolazo32@icloud.com>
2023-10-20 17:15:21 -0600

    Cambios en el css y el navBar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,20 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="/css/NavBarStyle.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
 
-<div style="width: 100%; height: 100%; position: relative;">
+<div class="cuerpo">
 
     <!--Barra superior-->
-    <div style="width: 100%; height: 81px; padding-top: 9px; padding-bottom: 9px; padding-left: 60px; padding-right: 1247px; left: 0px; top: 0px; position: absolute; background: white; box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.05); justify-content: flex-start; align-items: center; display: inline-flex">
+    <div class="BarraSuperior">
         <a href="/"><img style="width: 133px; height: 63px"src="imgLogos/LogoASEISNEWSLETRAS.png" /></a>
     </div>
 
     <!--Cuerpo de la pagina-->
-    <div style="width: 78%; height: 850px; left: 393px; top: 97px; position: absolute; background: white; border-radius: 16px; overflow: hidden">
+    <div class="contenido">
     @yield('content')
     </div>
 
@@ -55,5 +56,8 @@
             
     </div>
 </div>
+
+@yield('scripts')
+
 </body>
 </html>
