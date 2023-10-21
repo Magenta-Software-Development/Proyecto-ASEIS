fb63b59cf175dc5e9207d2d9686a9868735f383f
Danys Guevara <ga19014@ues.edu.sv>
2023-10-20 21:41:51 -0600

    Cambios de dany en vistas

---
+++ resources/views/gestion-usuarios/index-usuarios.blade.php
@@ -0,0 +1,10 @@
+
+@extends('Layouts.app')
+    @section('title', 'Gestion Usuarios')
+    @section('scripts')
+    @endsection
+    @section('css')
+    @endsection
+@section('content')
+    
+@endsection
