3dfe72c7935b378c0b75081cbf83bbe952a0b289
Danys Guevara <ga19014@ues.edu.sv>
2023-10-23 17:43:55 -0600

    cambios de danny

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 'resources/js/app.js', 'resources/css/NavbarStyle.css',
-                'resources/css/informacion.css', 'resources/js/load-file.js', 'resources/css/crearUsuarioAdminStyle.css',
-                'resources/css/index-usuarios.css', 'resources/css/styleUsuariosAdmin.css', 'resources/css/desactivarUsuario.css',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/NavbarStyle.css",
+                "resources/css/informacion.css",
+                "resources/js/load-file.js",
+                "resources/js/login.js",
+                "resources/css/crearUsuarioAdminStyle.css",
+                "resources/css/index-usuarios.css",
+                "resources/css/styleUsuariosAdmin.css",
+                "resources/css/desactivarUsuario.css",
             ],
             refresh: true,
         }),
