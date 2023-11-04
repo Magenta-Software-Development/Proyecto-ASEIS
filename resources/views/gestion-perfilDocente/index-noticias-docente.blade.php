35b3a211b06d04b7348b88847c7f56a50ea80c48
David Del Cid <david47.cruz@hotmail.com>
2023-11-04 17:30:57 -0600

    Borre una etiqueta de mas

--- resources/views/gestion-perfilDocente/index-noticias-docente.blade.php
+++ resources/views/gestion-perfilDocente/index-noticias-docente.blade.php
@@ -1,9 +1,9 @@
 @extends('Layouts.appDocente')
 @section('title', 'Gestion Docente')
 @section('scripts')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/40.0.0/classic/ckeditor.js"></script>
-</script>
+
 @endsection
 @section('css')
 @vite('resources/css/styleUsuariosAdmin.css')
