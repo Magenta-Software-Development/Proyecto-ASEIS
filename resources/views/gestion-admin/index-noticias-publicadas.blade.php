935020ec0e290ce22ad099aace2806641f3e7c96
Cesar Mauricio Hernadez Cubias <cheyosantos2468@gmail.com>
2023-11-03 22:06:10 -0600

    se solucionaron algunos errores con respecto a los botones de las vistas de admin, ademas se agrego el modal de mas informacion de noticias publicadas

--- resources/views/gestion-admin/index-noticias-publicadas.blade.php
+++ resources/views/gestion-admin/index-noticias-publicadas.blade.php
@@ -48,7 +48,7 @@
 
                 <div class="botonCurso botonFiltroActivoCurso">
                     <a href="#">
-                        <button>
+                        <button  data-bs-toggle="modal" data-bs-target="#modalMasInfo">
                             más información
                         </button>
                     </a>
@@ -71,6 +71,67 @@
         </div>
 
    </div>
+
+       <!-- Modal -->
+        <div class="modal fade" id="modalMasInfo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <!-- Encabezado de el modal -->
+                    <div class="modal-header">
+                        <h5 class="modal-title h4" id="exampleModalXlLabel"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"></font></font></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerca"></button>
+                    </div>
+                    <!-- cuerpo de el modal -->
+                    <div class="modal-body">
+
+                        <!-- Encabezado -->
+                        <div class="row">
+                            <div class="col-12 fuente-encabezado">
+                                <p>KDC El Salvador 2023</p>
+                            </div>
+                        </div>
+
+                        <!-- banner -->
+                        <div class="row">
+                            <div class="col-12">
+                                <img class="imagen-banner" src="{{ asset('images/Rectangle 56.png') }}">
+                            </div>
+                        </div>
+
+                        <!-- contenido de la noticia -->
+                        <div class="row">
+                            <div class="col-12 contenido-noticia">
+                                <p>Kubernetes Community Days es un encuentro diseñado para reunir a profesionales, entusiastas y curiosos de la tecnología que deseen explorar el mundo de Kubernetes. Será una jornada llena de charlas magistrales, talleres prácticos y oportunidades para establecer conexiones con expertos en la materia. Kubernetes Community Days es un encuentro diseñado para reunir a profesionales, entusiastas y curiosos de la tecnología que deseen explorar el mundo de Kubernetes. Será una jornada llena de charlas magistrales, talleres prácticos y oportunidades para establecer conexiones con expertos en la materia. Kubernetes Community Days es un encuentro diseñado para reunir a profesionales, entusiastas y curiosos de la tecnología que deseen explorar el mundo de Kubernetes. Será una jornada llena de charlas magistrales, talleres prácticos y oportunidades para establecer conexiones con expertos en la materia. Kubernetes Community Days es un encuentro diseñado para reunir a profesionales, entusiastas y curiosos de la tecnología que deseen explorar el mundo de Kubernetes. Será una jornada llena de charlas magistrales, talleres prácticos y oportunidades para establecer conexiones con expertos en la materia. Kubernetes Community Days es un encuentro diseñado para reunir a profesionales, entusiastas y curiosos de la tecnología que deseen explorar el mundo de Kubernetes. Será una jornada llena de charlas magistrales, talleres prácticos y oportunidades para establecer conexiones con expertos en la materia.</p>
+                            </div>
+
+                        </div>
+
+                        <!-- botones de editar y eliminar -->
+                        <div class="row mt-5">
+                            <div class="col-6 align-items-center justify-content-center d-flex">
+                                <button class="btn-editar-noticia">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M19.3 8.925L15.05 4.725L16.45 3.325C16.8333 2.94167 17.3043 2.75 17.863 2.75C18.4217 2.75 18.8923 2.94167 19.275 3.325L20.675 4.725C21.0583 5.10833 21.2583 5.571 21.275 6.113C21.2917 6.655 21.1083 7.11733 20.725 7.5L19.3 8.925ZM17.85 10.4L7.25 21H3V16.75L13.6 6.15L17.85 10.4Z" fill="white"/>
+                                </svg>
+                                    <p>Editar</p>
+                                </button>
+                            </div>
+
+                            <div class="col-6 align-items-center justify-content-center d-flex">
+                                <button class="btn-eliminar-noticia">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M7 21C6.45 21 5.979 20.804 5.587 20.412C5.195 20.02 4.99933 19.5493 5 19V6H4V4H9V3H15V4H20V6H19V19C19 19.55 18.804 20.021 18.412 20.413C18.02 20.805 17.5493 21.0007 17 21H7ZM9 17H11V8H9V17ZM13 17H15V8H13V17Z" fill="#FF0000"/>
+                                </svg>
+                                    <p>Eliminar</p>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
 
 </div>
 
