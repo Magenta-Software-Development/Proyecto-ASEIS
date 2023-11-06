2342fd7ecc511e7d50ca0dbf9bb1d5b65ebef084
Diegolazo32 <diegolazo32@icloud.com>
2023-11-05 23:16:31 -0600

    Orden y limpieza en el web.php

--- routes/web.php
+++ routes/web.php
@@ -40,35 +40,30 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 //Todas estas rutas deben de estar bloqueadas hasta que se haga un auth o un login
-//Route::middleware('auth')->group(function () {
+Route::controller(RouteController::class)->group(function () {
 
-    Route::controller(RouteController::class)->group(function () {
+    //Rutas de Admin ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    Route::get('/index', [RouteController::class, 'index'])->name('app_index'); //Muestra el indice del admin
+    Route::get('/AdministrarUsuarios/indexDocente', [RouteController::class, 'gestionIndexDocente'])->name('app_index_docentes'); //Muestra la pagina de gestion de docentes
+    Route::get('/AdministrarUsuarios/indexEstudiante', [RouteController::class, 'gestionIndexEstudiante'])->name('app_index_estudiantes'); //Muestra la pagina de gestion de estudiantes
+    Route::get('/AdministrarCursos/indexCursosNoDisponibles', [RouteController::class, 'gestionCursosNoDisponibles'])->name('app_index_cursos_no_disponibles'); //Muestra la pagina de gestion de cursos no disponibles
+    Route::get('/AdministrarCursos/indexCursosPublicados', [RouteController::class, 'gestionCursosDisponibles'])->name('app_index_cursos_publicados'); //Muestra la pagina de gestion de cursos publicados
+    Route::get('/AdministrarCursos/indexCrearCurso', [RouteController::class, 'gestionCursos'])->name('app_index_gestion_cursosAdmin'); //Muestra el indice de gestion de crear noticas del administrador.
+    Route::get('/AdministrarCategorias/indexCategorias', [RouteController::class, 'gestionCategorias'])->name('app_index_categorias'); //Muestra la pagina de gestion de categorias
+    Route::get('/AdministrarEspecialidades/indexEspecialidades', [RouteController::class, 'gestionEspecialidades'])->name('app_index_especialidades'); //Muestra la pagina de gestion de especialidades
+    Route::get('/AdministrarCodigos/indexCodigos', [RouteController::class, 'gestionCodigos'])->name('app_index_codigos'); //Muestra la pagina de gestion de codigos
+    Route::get('/AdministrarNoticias/indexGestionNoticias', [RouteController::class, 'gestionNoticias'])->name('app_index_gestion_noticias'); //Muestra el indice de gestion de usuarios de docentes
+    Route::get('/AdministrarNoticias/indexNoticias', [RouteController::class, 'gestionIndexNoticias'])->name('app_index_noticias'); //Muestra el indice de gestion de crear noticas del administrador.
 
-        //Rutas de Admin ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        Route::get('/index', [RouteController::class, 'index'])->name('app_index'); //Muestra el indice del admin
-        Route::get('/GestionarUsuarios/indexDocente', [RouteController::class, 'gestionIndexDocente'])->name('app_index_docentes'); //Muestra la pagina de gestion de docentes
-        Route::get('/GestionarUsuarios/indexEstudiante', [RouteController::class, 'gestionIndexEstudiante'])->name('app_index_estudiantes'); //Muestra la pagina de gestion de estudiantes
-        Route::get('/gestionAdmin/indexCursosNoDisponibles', [RouteController::class, 'gestionCursosNoDisponibles'])->name('app_index_cursos_no_disponibles'); //Muestra la pagina de gestion de cursos no disponibles
-        Route::get('/gestionAdmin/indexCursosPublicados', [RouteController::class, 'gestionCursosDisponibles'])->name('app_index_cursos_publicados'); //Muestra la pagina de gestion de cursos publicados
-        Route::get('/GestionarCategorias/indexCategorias', [RouteController::class, 'gestionCategorias'])->name('app_index_categorias'); //Muestra la pagina de gestion de categorias
-        Route::get('/GestionarEspecialidades/indexCategorias', [RouteController::class, 'gestionEspecialidades'])->name('app_index_especialidades'); //Muestra la pagina de gestion de especialidades
-        Route::get('/GestionarCodigos/indexCategorias', [RouteController::class, 'gestionCodigos'])->name('app_index_codigos'); //Muestra la pagina de gestion de codigos
-        Route::get('/gestionAdmin/indexGestionNoticias', [RouteController::class, 'gestionNoticias'])->name('app_index_gestion_noticias'); //Muestra el indice de gestion de usuarios de docentes
-        Route::get('/gestionAdmin/indexGestionCursos', [RouteController::class, 'gestionCursos'])->name('app_index_gestion_cursosAdmin'); //Muestra el indice de gestion de crear noticas del administrador.
-        Route::get('/gestionAdmin/indexNoticias', [RouteController::class, 'gestionIndexNoticias'])->name('app_index_noticias'); //Muestra el indice de gestion de crear noticas del administrador.
+    //Rutas de Docente -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    Route::get('/indexD', [RouteController::class, 'indexD'])->name('app_index_docente'); //Muestra el indice del docente
+    Route::get('/Docente/indexperfilDocente', [RouteController::class, 'perfilDocente'])->name('app_perfil_docente'); //Muestra el perfil del docente
+    Route::get('/Docente/indexCrearCurso', [RouteController::class, 'indexCrearCurso'])->name('app_index_gestion_cursos'); //Muestra la pagina de gestion de cursos creados por ese docente en especifico
+    Route::get('/Docente/indexCursosPublicadosDocente', [RouteController::class, 'CursosPublicadosDocente'])->name('app_index_cursosPublicados_docente'); //Muestra la pagina de gestion de cursos publicados por ese docente en especifico
+    Route::get('/Docente/indexCursosNoDisponiblesDocente', [RouteController::class, 'CursosNoDisponiblesDocente'])->name('app_index_cursos_no_disponibles_docente'); //Muestra la pagina de gestion de cursos no disponibles por ese docente en especifico
+    Route::get('/Docente/indexNoticiasPublicadas', [RouteController::class, 'NoticiasPublicadas'])->name('app_index_noticias_publicadas'); //Muestra la pagina de gestion de noticias publicadas por ese docente en especifico
+    Route::get('/Docente/indexCrearNoticia', [RouteController::class, 'GestionNoticiasDocente'])->name('app_index_gestion_noticias_docente'); //Muestra el indice de gestion de noticias
 
+    //Route::get('TU RUTA', [RouteController::class, 'NOMBRE DE LA FUNCION'])->name('NOMBRE DE LLAMADO DE LA RUTA'); -> IR A ROUTE CONTROLLER A CONFIGURAR TU FUNCION
 
-        //Rutas de Docente -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-        Route::get('/indexD', [RouteController::class, 'indexD'])->name('app_index_docente'); //Muestra el indice del docente
-        Route::get('/indexD/perfilDocente', [RouteController::class, 'perfilDocente'])->name('app_perfil_docente'); //Muestra el perfil del docente
-        Route::get('/gestionDocente/indexCrearCurso', [RouteController::class, 'indexCrearCurso'])->name('app_index_gestion_cursos'); //Muestra la pagina de gestion de cursos creados por ese docente en especifico
-        Route::get('/gestionDocente/indexCursosPublicadosDocente', [RouteController::class, 'CursosPublicadosDocente'])->name('app_index_cursosPublicados_docente'); //Muestra la pagina de gestion de cursos publicados por ese docente en especifico
-        Route::get('/gestionDocente/indexCursosNoDisponiblesDocente', [RouteController::class, 'CursosNoDisponiblesDocente'])->name('app_index_cursos_no_disponibles_docente'); //Muestra la pagina de gestion de cursos no disponibles por ese docente en especifico
-        Route::get('/gestionDocente/indexNoticiasDocente', [RouteController::class, 'NoticiasDocente'])->name('app_index_noticias_docente'); //Muestra la pagina de crear noticias para ese docente en especifico
-        Route::get('/gestionDocente/indexNoticiasPublicadas', [RouteController::class, 'NoticiasPublicadas'])->name('app_index_noticias_publicadas'); //Muestra la pagina de gestion de noticias publicadas por ese docente en especifico
-        Route::get('/gestionDocente/indexGestionNoticiasD', [RouteController::class, 'GestionNoticiasDocente'])->name('app_index_gestion_noticias_docente'); //Muestra el indice de gestion de noticias
-
-        //Route::get('TU RUTA', [RouteController::class, 'NOMBRE DE LA FUNCION'])->name('NOMBRE DE LLAMADO DE LA RUTA'); -> IR A ROUTE CONTROLLER A CONFIGURAR TU FUNCION
-
-    });
-//});
+});
